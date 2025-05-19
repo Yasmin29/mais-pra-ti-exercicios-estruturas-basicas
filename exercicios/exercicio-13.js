0dce4cbc1ccae3b0ae06f7b0cc2f6d5952052ceb
Yasmin Lippert <yasmin.lippert@hotmail.com>
2025-05-19 15:51:07 -0300

    resolução do exercicio 13

--- exercicios/exercicio-13.js
+++ exercicios/exercicio-13.js
@@ -1,12 +1,19 @@
 // Fazer um algoritmo para receber números decimais até que o usuário digite 0 e fazer
 // a média aritmética desses números.
+
 prompt = require('prompt-sync')()
 
 let listaNumeros = []
-let soma = 0
-let media = 0
-let valorEntrada = parseFloat(prompt('Digite o valor: '))
+let somaTotal = 0
+let valorEntrada = null
 
 while(valorEntrada != 0){
-    console.log('testes')
+    valorEntrada = parseFloat(prompt('Digite o valor: '))
+    if (valorEntrada != 0){
+        listaNumeros.push(valorEntrada)
+        somaTotal += valorEntrada
+    }    
 }
+
+let media = somaTotal / listaNumeros.length
+console.log('Média:', media)
